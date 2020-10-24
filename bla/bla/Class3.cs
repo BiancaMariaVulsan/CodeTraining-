6ac73a861a65ac7eb284b56732fa022b62307277
unknown <biavulsan@yahoo.com>
2020-10-24 12:42:02 +0300

    Created blablablabla

---
+++ bla/bla/Class3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bla
+{
+    class Class3
+    {
+        private void blablablabla()
+        {
+
+        }
+    }
+}
