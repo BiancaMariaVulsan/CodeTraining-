e1f580bfeb001f113595018c9f703eb82e8d2d17
unknown <biavulsan@yahoo.com>
2020-10-24 12:39:01 +0300

    Created blablabla

---
+++ bla/bla/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bla
+{
+    class Class2
+    {
+        private void blablabla()
+        {
+
+        }
+    }
+}
